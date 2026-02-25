a88dc7e5ee22e50242c3f8b760df152b8afd55a5
segfaultscribe <segfaultscribe@gmail.com>
2026-02-25 21:45:13 +0530

    remove batchID from Job

--- internal/types.go
+++ internal/types.go
@@ -4,7 +4,6 @@ import "encoding/json"
 
 type Job struct {
 	EventID     string          `json:"event_id"`
-	BatchId     string          `json:"batch_id"`
 	Payload     json.RawMessage `json:"payload"`
 	RetryCount  int             `json:"retry_count"`
 	Destination string          `json:"destination"`
